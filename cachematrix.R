6fe80647a8c1834718d0bf1f51fd375be7bc712b
mckune929 <mckune929@gmail.com>
2016-10-10 22:02:59 -0500

    Completed Week 3 Assignment
    Completed R code and documentation for the makeCacheMatrix and
    cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions save clock cycles when repetitively calculating the input
+## of a square matrix. This is accomplished by creating a cache in which the inverse is saved. So long as 
+## the input does not change, the inverse matrix will simply be pulled from memory, rather than repeatedly
+## (and unnecessarily) recalculated.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a square, invertible matrix as its input. The function returns a list of four
+## functions to set the input matrix, pull the input matrix from the cache, set the inverse of the input
+## matrix, and pull the inverse matrix from the cache. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverseMatrix <- NULL
+        setInput <- function(y) {
+                x <<- y
+                inverseMatrix <<- NULL
+        }
+        getInput <- function() x
+        setInverse <- function(inverseValue) {
+                inverseMatrix <<- inverseValue
+        }
+        getInverse <- function() inverseMatrix
+        list (setInput = setInput, getInput = getInput, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve attempts to pull the cached inverse of the input matrix, and returns the cached value
+## if the inverse matrix is not null. If the inverse matrix is null, cacheSolve calculates the inverse,
+## saves it to the cache, and returns the inverse matrix as the only output.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverseMatrix <- x$getInverse()
+        if(!is.null(inverseMatrix)) {
+                message("Pulling cached inverse matrix")
+                return(inverseMatrix)
+        }
+        inputMatrix <- x$getInput()
+        inverseMatrix <- solve(inputMatrix, ...)
+        x$setInverse(inverseMatrix)
 }
